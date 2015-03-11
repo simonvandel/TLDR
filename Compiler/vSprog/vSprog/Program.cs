5b69461b60f5b28c4a876740fc8f774155cbcb64
Simon Vandel Sillesen <simon.vandel@gmail.com>
2015-03-11 12:08:14 +0100

    kompiler grammar direkte fra program

--- Compiler/vSprog/vSprog/Program.cs
+++ Compiler/vSprog/vSprog/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hime.Redist;
+using Hime.CentralDogma;
 
 namespace vSprog
 {
@@ -11,77 +12,63 @@ namespace vSprog
     {
         static void Main(string[] args)
         {
-            System.IO.StreamReader reader = new System.IO.StreamReader("../../Parser/code.txt");
+			CompilationTask task = new CompilationTask ();
+			task.AddInputFile ("../../Parser/vSprog.gram");
+			task.Mode = Hime.CentralDogma.Output.Mode.Assembly;
+			task.Execute ();
+			Hime.CentralDogma.SDK.AssemblyReflection assembly = new Hime.CentralDogma.SDK.AssemblyReflection("vSprog.dll");
 
-            vSprogLexer lexer = new vSprogLexer(reader);
-            Console.WriteLine("String lexed.. Parsing");
-            MyActions myActions = new MyActions(); 
-            vSprogParser parser = new vSprogParser(lexer, myActions);
+            //System.IO.StreamReader reader = new System.IO.StreamReader("../../Parser/code.txt");
+
+			const string input = 
+				"( 2 + 3 ) * 4;\n" +
+				"( 3 * 4 );" +
+				"let fn ( x ) : int -> int = x + 2;";
+				
+			var dict = new Dictionary<string, SemanticAction> ();
+			dict ["SimpleType"] = (x,y) => {};
+			dict ["OnProgram"] = (x,y) => {};
+
+			Hime.Redist.Parsers.IParser parser = null;
+			try {
+				parser = assembly.GetParser<String> (input,dict);
+			} catch (Exception) {
+				throw new Exception ("Not all semantic actions declared in grammar was assigned in code. Check dictionary containing semantic actions.");
+			}
+
             ParseResult result = parser.Parse();
+
             foreach (var error in result.Errors)
             {
                 Console.WriteLine(error.Message);
             }
-            //Console.WriteLine("Parse Completed: {0}", myActions.StructString.Pop());
+			PrintTree (result.Root, 0);
             Console.ReadLine();
         }
-    }
-    class MyActions : vSprogParser.Actions
-    {
-        public Queue<string> StatementString = new Queue<string>();
-        public Queue<string> DeclarationString = new Queue<string>();
-        private Queue<string> StructString = new Queue<string>();
-        private Queue<string> TypeDeclsString = new Queue<string>();
-        private Queue<string> TypeDeclString = new Queue<string>();
-        private Queue<string> SimpleTypeString = new Queue<string>();
-        public override void EndStatementList(Symbol head, SemanticBody body)
-        {
-            string tmpString = StatementString.Dequeue() + ";";
-            StatementString.Enqueue(tmpString);
-            Console.WriteLine(tmpString);
-        }
-        public override void DeclarationStatement(Symbol head, SemanticBody body)
-        {
-            StatementString.Enqueue(DeclarationString.Dequeue());
-        }
-        public override void StructDeclaration(Symbol head, SemanticBody body)
-        {
-            DeclarationString.Enqueue(StructString.Dequeue());
-        }
-        public override void StructProduction(Symbol head, SemanticBody body)
-        {
-            string tmpString = string.Format("{0} {1} {2} {3} {4} {5} {6}",
-                               body[0].Value, body[1].Value, body[2].Value, body[3].Value, body[4].Value, TypeDeclsString.Dequeue(), body[6].Value);
-            StructString.Enqueue(tmpString);
-            Console.WriteLine(tmpString);
-        }
 
-        public override void TypeDecls(Symbol head, SemanticBody body)
-        {
-            string tmpString = TypeDeclString.Dequeue() + " " + body[1].Value + " " + TypeDeclString.Dequeue();
-            TypeDeclsString.Enqueue(tmpString);
-            Console.WriteLine(tmpString);
-        }
-        public override void TypeDecl(Symbol head, SemanticBody body)
-        {
-            string tmpString = body[0].Value + body[1].Value + SimpleTypeString.Dequeue();
-            TypeDeclString.Enqueue(tmpString);
-            Console.WriteLine(tmpString);
-        }
-        public override void SimpleType(Symbol head, SemanticBody body)
-        {
-            string tmpString = body[0].Value;
-            SimpleTypeString.Enqueue(tmpString);
-            Console.WriteLine(tmpString);
-        }
-        public void PrettyPrint(Symbol head, SemanticBody body)
-        {
-            Console.Write(head.Name + " -> ");
-            for (int i = 0; i < body.Length; i++)
-            {
-                Console.Write(body[i].Value + " ");
-            }
-            Console.WriteLine();
-        }
+		public static void PrintTree(ASTNode node, int level) {
+			// print level
+			for (int i = 0; i < level; i++) {
+				Console.Write ("--");
+			}
+			if (level != 0) {
+				Console.Write (' ');
+			}
+				
+			Console.WriteLine (node.Symbol.Value);
+			foreach (var child in node.Children) {
+				PrintTree (child, level + 1);
+			}
+		}
+
+		public static void PrettyPrint(Symbol head, SemanticBody body)
+		{
+			Console.Write(head.Name + " -> ");
+			for (int i = 0; i < body.Length; i++)
+			{
+				Console.Write(body[i].Value + " ");
+			}
+			Console.WriteLine();
+		}
     }
 }
