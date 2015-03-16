14c3b0240b37b7bdf7043003230e2ba66d27fe30
Simon Vandel Sillesen <simon.vandel@gmail.com>
2015-03-16 19:41:26 +0100

    prut

--- Compiler/vSprog/fsharpTry/Program.fs
+++ Compiler/vSprog/fsharpTry/Program.fs
@@ -24,6 +24,56 @@ type DecoratedAST =
     | Leaf of DecoratedASTNode
     | Tree of DecoratedAST list
 
+type Val =
+    | Int of int
+    | Real of float
+    | Char of char
+    | Bool of bool
+    | List of Val list
+
+type TypeSymbol = String * Val // (id, value)
+
+type Scope = 
+    {
+        SymbolList:TypeSymbol list
+        Parent:Scope
+    }
+
+type Ex =
+    | Variable of string * string // * Scope // (name, type)
+    | Int of int
+    | Real of float
+
+type AST = 
+    | Program of AST list
+    //| Assignment of TypeSymbol * Scope
+    | Assignment of Ex * Ex
+
+let toInternalEx (ast:ASTNode) : Ex =
+    match ast.Symbol.Value with
+    | value when System.Int32.TryParse(value,ref 0) -> Int (System.Int32.Parse(value))
+    | value when System.Double.TryParse(value,ref 0.0) -> Real (float value)
+    | value -> // must be variable
+        let id = value
+        let typeId = (ast.Children.Item 0).Symbol.Value
+        Variable (id, typeId)
+
+let toInternalAST (ast:ASTNode) : AST =
+    match ast.Symbol.Value with
+    | "Statement" -> 
+        match (ast.Children.Item 0).Symbol.Value with
+        | "Initialisation" -> 
+            let curNode = ast.Children.Item 0
+            let lhs = toInternalEx (curNode.Children.Item 1)
+            let rhs = toInternalEx (curNode.Children.Item 2)
+
+            Assignment(lhs, rhs)
+        | _ -> 
+            printf "%s" "Var andet end declaration"
+            Assignment(Int 1, Int 1)
+    | _ -> Assignment(Int 0, Int 0)
+
+
 let levelToString level : String =
     [0..level-1]
     |> Seq.collect (fun l -> sprintf "%s" "--")
@@ -59,14 +109,14 @@ let main argv =
     let report = task.Execute()
     let assembly = SDK.AssemblyReflection "vSprogGrammar.dll"
 
-    let input = "a := 2;"
+    let input = "let a:int := 2.5;"
     let dictionary = new Dictionary<string,SemanticAction>(dict [
                                                                  ("SimpleType", new SemanticAction(test));
                                                                  ("OnProgram", new SemanticAction(test))
                                                                  ])
 
     let parseResult = try
-                          Some ((assembly.GetParser<String> (input,dictionary)).Parse())
+                          Some ((assembly.GetParser<String> (input)).Parse())
                       with
                           | :? TargetInvocationException -> None
     
@@ -75,8 +125,13 @@ let main argv =
       printfn "%s" "Parsing success. No errors"
       //printTree res.Root 0
       res.Root 
-      |> createDecoratedAST
-      |> fun dAST -> prettyPrintDecoratedAST dAST 0
+      |> fun r -> printTree r 0
+
+      res.Root
+      |> toInternalAST
+      |> printf "%A"
+      //|> createDecoratedAST
+      //|> fun dAST -> prettyPrintDecoratedAST dAST 0
     | Some res -> 
       // print all error messages
       res.Errors |> Seq.iter (fun err -> printfn "%O" (err))
