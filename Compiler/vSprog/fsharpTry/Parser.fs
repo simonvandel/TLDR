120ffcac949538965f8ae48ce74e0d35da7a3114
cnHeider <mrnaah@gmail.com>
2015-03-24 09:05:25 +0100

    edit filepath

--- Compiler/vSprog/fsharpTry/Parser.fs
+++ Compiler/vSprog/fsharpTry/Parser.fs
@@ -22,7 +22,7 @@ module Parser =
 
     let parse (srcInput:string) : Result<ASTNode> =
         let task = CompilationTask ()
-        task.AddInputFile "../../grammar"
+        task.AddInputFile "../../grammar.gram"
         task.Mode <- Hime.CentralDogma.Output.Mode.Assembly
         let report = task.Execute()
         let assembly = SDK.AssemblyReflection "vSprogGrammar.dll"
